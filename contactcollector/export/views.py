881994135559f5d1c992b8d8c5e7875a7c0396c0
Peter Reutemann <fracpete@gmail.com>
2017-11-08 23:17:29 +1300

    using to worksheets now: contact, job

--- contactcollector/export/views.py
+++ contactcollector/export/views.py
@@ -16,9 +16,7 @@ def index(request):
 
 
 def download(request):
-
-    # read data
-    header = [
+    header_cont = [
         'survey_name',
         'survey_notes',
         'employer_name',
@@ -29,23 +27,31 @@ def download(request):
         'employee_last',
         'employee_email',
         'employee_notes',
+    ]
+    body_cont = []
+    header_job = [
+        'survey_name',
+        'survey_notes',
+        'employer_name',
+        'employer_address',
+        'employer_notes',
         'job_title',
-        'job_tech_skills',
-        'job_deliverables',
         'job_description',
         'job_fulltime',
+        'job_tech_skills',
+        'job_responsibilities',
+        'job_deliverables',
         'job_categories',
     ]
-    body = []
+    body_job = []
 
     rs_s = Survey.objects.all()
     for r_s in rs_s:
-        row = []
-        body.append(row)
         rs_er = Employer.objects.all().filter(survey=r_s.id)
         for r_er in rs_er:
             rs_em = Employee.objects.all().filter(employer=r_er.id)
             for r_em in rs_em:
+                row = []
                 row.append(r_s.name)
                 row.append(r_s.notes)
                 row.append(r_er.name)
@@ -56,9 +62,30 @@ def download(request):
                 row.append(r_em.last_name)
                 row.append(r_em.email)
                 row.append(r_em.notes)
+                body_cont.append(row)
+
+            rs_j = Job.objects.all().filter(employer=r_er.id)
+            for r_j in rs_j:
+                row = []
+                row.append(r_s.name)
+                row.append(r_s.notes)
+                row.append(r_er.name)
+                row.append(r_er.address)
+                row.append(r_er.notes)
+                row.append(r_j.title)
+                row.append(r_j.description)
+                row.append(r_j.full_time)
+                row.append(r_j.technical_skills)
+                row.append(r_j.responsibilities)
+                row.append(r_j.deliverables)
+                row.append(r_j.categories)
+                body_job.append(row)
 
     # generate output
-    book = excel.pe.Book({'Contacts': [header] + body})
+    book = excel.pe.Book({
+        'Contacts': [header_cont] + body_cont,
+        'Jobs': [header_job] + body_job,
+    })
     response = excel.make_response(book, "xls", file_name="contactcollector.xls")
     return response
 
